53913e8475dfec63270e69488b5ef5b5a496920c
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2026-05-12 03:16:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,66 @@ function getLuckyNumber()
     return getRandMinMax(1, 999);
 }
 
-function initalize()
+function newLuckyNumber()
 {
-    document.getElementById("lucky").textContent = getLuckyNumber();
+    let luckyNumber = getLuckyNumber();
+    localStorage.setItem("epoch", Date.now());
+    localStorage.setItem("luckyNumber", luckyNumber);
+    document.getElementById("lucky").textContent = luckyNumber;
+}
+
+function initialize()
+{
+    let luckyNumber;
+
+    if(checkStorage() === true)
+    {
+        if(localStorage.getItem("epoch") === null)
+        {
+            newLuckyNumber();
+        }
+        else
+        {
+            let previousEpoch = parseInt(localStorage.getItem("epoch"));
+            let currentEpoch = parseInt(Date.now());
+
+            if(currentEpoch - previousEpoch > 86400000)
+            {
+                newLuckyNumber();
+            }
+            else
+            {
+                if(localStorage.getItem("luckyNumber") === null)
+                {
+                   newLuckyNumber();
+                }
+                else
+                {
+                    luckyNumber = localStorage.getItem("luckyNumber");
+                    document.getElementById("lucky").textContent = luckyNumber;
+                }
+            }
+        }
+    }
+    else
+    {
+        luckyNumber = getLuckyNumber();
+        document.getElementById("lucky").textContent = luckyNumber;
+    }
+}
+
+function checkStorage()
+{
+    var test = "test";
+
+    try 
+    {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } 
+    catch(e)
+    {
+        return false;
+    }
 }
