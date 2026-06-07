5f5f65fd40acb4ccf78472bdfb4cf5a244e2b268
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2026-06-06 19:19:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -134,16 +134,6 @@ function randomizeColors()
     document.querySelector("link[rel=\"shortcut icon\"]").href = selectedTheme.favicon;
 }
 
-function hexToRGB(hex) {
-    hex = hex.replace("#", "");
-    
-    const r = parseInt(hex.substring(0, 2), 16);
-    const g = parseInt(hex.substring(2, 4), 16);
-    const b = parseInt(hex.substring(4, 6), 16);
-    
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-}
-
 function checkStorage()
 {
     var test = "test";
